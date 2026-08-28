45987b15d33054272e20073ff33cc19e714966e8
Makom64 <yellowffd400@gmail.com>
2026-08-28 10:26:29 +0900

    [Feat] Kiosk 클래스 생성

---
+++ 260828_노승훈_평가/260828_노승훈_평가/Kiosk.cs
@@ -0,0 +1,9 @@
+﻿public class Kiosk
+{
+    private MenuInfo menu;
+
+    public Kiosk(string name, int price, PriceState priceState, int amount)
+    {
+        menu = new MenuInfo(name, price, priceState, amount);
+    }
+}
--- 260828_노승훈_평가/260828_노승훈_평가/MenuInfo.cs
+++ 260828_노승훈_평가/260828_노승훈_평가/MenuInfo.cs
@@ -2,4 +2,14 @@
 {
     private string _name;
     private int _price;
+    private PriceState _priceState;
+    private int _amount;
+
+    public MenuInfo(string name, int price, PriceState priceState, int amount)
+    {
+        _name = name;
+        _price = price;
+        _priceState = priceState;
+        _amount = amount;
+    }
 }
