cf417c8828724d928d318eca66744449251928e5
Makom64 <yellowffd400@gmail.com>
2026-08-28 13:40:12 +0900

    [Feat] 본문에 todayMenu 배열 생성

--- 260828_노승훈_평가/260828_노승훈_평가/Program.cs
+++ 260828_노승훈_평가/260828_노승훈_평가/Program.cs
@@ -9,6 +9,13 @@ public class Program
         Console.WriteLine($"{STORE} 키오스크");
         Console.WriteLine("--------------------");
         Console.WriteLine("[메뉴판]");
+        CafeMenu[] todayMenu = new CafeMenu[5];
+        todayMenu[0] = new Drink("아메리카노", 2000, SaleState.Regular);
+        todayMenu[1] = new Drink("카페라때", 2000, SaleState.Regular);
+        todayMenu[2] = new Drink("아이스티", 2000, SaleState.Regular);
+        todayMenu[3] = new Desert("초코칩 쿠키", 2000, SaleState.Regular);
+        todayMenu[4] = new Desert("크림치즈 어니언 베이글", 2000, SaleState.Regular);
+        
         Console.WriteLine("--------------------");
         Console.WriteLine("[장바구니]");
         Console.WriteLine("--------------------");
