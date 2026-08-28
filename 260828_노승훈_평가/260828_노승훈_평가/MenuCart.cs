df2a2e69d9a416b3dff2384be7464b2e5a02d304
Makom64 <yellowffd400@gmail.com>
2026-08-28 15:04:03 +0900

    [Feat] 카트에 담기, 카트 비우기 인터페이스 삭제

--- 260828_노승훈_평가/260828_노승훈_평가/MenuCart.cs
+++ 260828_노승훈_평가/260828_노승훈_평가/MenuCart.cs
@@ -1,9 +1,22 @@
 ﻿public class MenuCart<T>
 {
-    private List<T> menuCart;
+    private List<T> menuCart; // 장바구니 배열
+    private int AddedMenu; // 더한 메뉴
     
-    public MenuCart(CafeMenu cafeMenu)
+    public MenuCart(int count)
     {
-        menuCart = new List<T>();
+        menuCart = new List<T>(count);
+        AddedMenu = count; // 담은 메뉴 = 장바구니 크기
+    }
+
+    // CafeMenu(무엇인지 모름)을 배열에 담아주는 메서드
+    public void AddtoCart(T menu)
+    {
+        menuCart.Add(menu);
+    }
+
+    public T Get(int number) // 번호를 받는다, 반환형이 T일것
+    {
+        return menuCart[number];
     }
 }
--- 260828_노승훈_평가/260828_노승훈_평가/Program.cs
+++ 260828_노승훈_평가/260828_노승훈_평가/Program.cs
@@ -9,6 +9,7 @@ public class Program
         Console.WriteLine($"{STORE} 키오스크");
         Console.WriteLine("--------------------");
         Console.WriteLine("[메뉴판]");
+        // 카페메뉴 생성
         CafeMenu[] todayMenu = new CafeMenu[5];
         todayMenu[0] = new Drink("아메리카노", 2000, SaleState.Regular);
         todayMenu[1] = new Drink("카페라때", 2000, SaleState.Regular);
@@ -16,6 +17,7 @@ public class Program
         todayMenu[3] = new Desert("초코칩 쿠키", 2000, SaleState.Regular);
         todayMenu[4] = new Desert("크림치즈 어니언 베이글", 2000, SaleState.Regular);
 
+        // 메뉴 출력
         for (int i = 0; i < todayMenu.Length; i++)
         {
             Console.Write($"{i + 1}. ");
@@ -24,6 +26,14 @@ public class Program
         
         Console.WriteLine("--------------------");
         Console.WriteLine("[장바구니]");
+        // 장바구니 생성
+        List<CafeMenu> menuCart = new List<CafeMenu>(5);
+        
+        // 장바구니에 메뉴를 담는다.
+        menuCart.AddtoCart(todayMenu[0]);
+        
+        
+        
         Console.WriteLine("--------------------");
         
         
