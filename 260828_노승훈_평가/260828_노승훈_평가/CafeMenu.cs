44ee8fed281563f5daa1a766fd397aae27931d98
Makom64 <yellowffd400@gmail.com>
2026-08-28 13:36:11 +0900

    [Feat] 기반클래스 CafeMenu 재생성

--- 260828_노승훈_평가/260828_노승훈_평가/CafeMenu.cs
+++ 260828_노승훈_평가/260828_노승훈_평가/CafeMenu.cs
@@ -1,4 +1,19 @@
 ﻿public class CafeMenu
 {
-    
+    public string _name { get; protected set; }
+    public int _price { get; protected set; }
+    public SaleState _state { get; protected set; }
+
+    public CafeMenu(string name, int price, SaleState state)
+    {
+        _name = name;
+        _price = price;
+        _state = state;
+    }
+}
+
+public enum SaleState
+{
+    Regular,
+    Discount
 }
